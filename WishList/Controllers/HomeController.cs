d8271f534d9d369dc7c2bd3bafcd34df81d059b4
punamg97 <65148417+punamg97@users.noreply.github.com>
2022-08-31 12:27:10 +0530

    Create HomeController.cs
    git add . && git commit -m 'Completed module'

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
